e4e9cfafa7987bc76bd29f8e37eed11f733e6c28
John William Davis <jwdavisdeveloper@gmail.com>
2023-08-21 00:13:09 -0700

    complete 1st exercise - prior to looking at solution

--- src/helpers/file-helpers.js
+++ src/helpers/file-helpers.js
@@ -8,43 +8,32 @@ export async function getBlogPostList() {
   const blogPosts = [];
 
   for (let fileName of fileNames) {
-    const rawContent = await readFile(
-      `/content/${fileName}`
-    );
+    const rawContent = await readFile(`/content/${fileName}`);
 
     const { data: frontmatter } = matter(rawContent);
 
     blogPosts.push({
       slug: fileName.replace('.mdx', ''),
       ...frontmatter,
+      id: crypto.randomUUID(),
     });
   }
 
-  return blogPosts.sort((p1, p2) =>
-    p1.publishedOn < p2.publishedOn ? 1 : -1
-  );
+  return blogPosts.sort((p1, p2) => (p1.publishedOn < p2.publishedOn ? 1 : -1));
 }
 
 export async function loadBlogPost(slug) {
-  const rawContent = await readFile(
-    `/content/${slug}.mdx`
-  );
+  const rawContent = await readFile(`/content/${slug}.mdx`);
 
-  const { data: frontmatter, content } =
-    matter(rawContent);
+  const { data: frontmatter, content } = matter(rawContent);
 
   return { frontmatter, content };
 }
 
 function readFile(localPath) {
-  return fs.readFile(
-    path.join(process.cwd(), localPath),
-    'utf8'
-  );
+  return fs.readFile(path.join(process.cwd(), localPath), 'utf8');
 }
 
 function readDirectory(localPath) {
-  return fs.readdir(
-    path.join(process.cwd(), localPath)
-  );
+  return fs.readdir(path.join(process.cwd(), localPath));
 }
