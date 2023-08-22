f65da94b01d82695e41d7afc2743a3967935a88b
John William Davis <jwdavisdeveloper@gmail.com>
2023-08-21 22:34:09 -0700

    finish exercise 4

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -2,22 +2,26 @@ import React from 'react';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import BlogHero from '@/components/BlogHero';
 import { loadBlogPost } from '@/helpers/file-helpers';
+import CodeSnippet from '@/components/CodeSnippet/CodeSnippet';
+import { BLOG_TITLE } from '@/constants';
 
 import styles from './postSlug.module.css';
 
+export async function generateMetadata({ params }) {
+  const { frontmatter } = await loadBlogPost(params.postSlug);
+  return {
+    title: `${frontmatter.title} - ${BLOG_TITLE}`,
+    description: frontmatter.abstract,
+  };
+}
+
 async function BlogPost({ params }) {
   const { frontmatter, content } = await loadBlogPost(params.postSlug);
-  // const data = await loadBlogPost(params.postSlug);
-  // const { frontMatter, content } = await compileMDX({
-  //   source: data,
-  //   options: { parseFrontmatter: true },
-  // });
-
   return (
     <article className={styles.wrapper}>
       <BlogHero title={frontmatter.title} publishedOn={frontmatter.publishedOn} />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote source={content} components={{ pre: CodeSnippet }} />
       </div>
     </article>
   );
--- src/helpers/file-helpers.js
+++ src/helpers/file-helpers.js
@@ -1,4 +1,5 @@
 import fs from 'fs/promises';
+import React from 'react';
 import path from 'path';
 import matter from 'gray-matter';
 
@@ -21,13 +22,13 @@ export async function getBlogPostList() {
   return blogPosts.sort((p1, p2) => (p1.publishedOn < p2.publishedOn ? 1 : -1));
 }
 
-export async function loadBlogPost(slug) {
+export const loadBlogPost = React.cache(async function loadBlogPost(slug) {
   const rawContent = await readFile(`/content/${slug}.mdx`);
 
   const { data: frontmatter, content } = matter(rawContent);
 
   return { frontmatter, content };
-}
+});
 
 function readFile(localPath) {
   return fs.readFile(path.join(process.cwd(), localPath), 'utf8');
