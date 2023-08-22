1fd4d1aa6b7418584620e3fcadbc23c62fc7cb1b
John William Davis <jwdavisdeveloper@gmail.com>
2023-08-21 17:26:40 -0700

    fix problems after watching solution - on the right track, but missing something

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import { MDXRemote } from 'next-mdx-remote/rsc';
-import { compileMDX } from 'next-mdx-remote/rsc';
 import BlogHero from '@/components/BlogHero';
 import { loadBlogPost } from '@/helpers/file-helpers';
 
 import styles from './postSlug.module.css';
 
 async function BlogPost({ params }) {
-  // const { frontMatter, content } = await loadBlogPost(params.postSlug);
-  const data = await loadBlogPost(params.postSlug);
-  const { frontMatter, content } = await compileMDX({
-    source: data,
-    options: { parseFrontmatter: true },
-  });
+  const { frontmatter, content } = await loadBlogPost(params.postSlug);
+  // const data = await loadBlogPost(params.postSlug);
+  // const { frontMatter, content } = await compileMDX({
+  //   source: data,
+  //   options: { parseFrontmatter: true },
+  // });
 
-  console.log(frontMatter);
-  console.log(content);
   return (
     <article className={styles.wrapper}>
-      <BlogHero title={frontMatter.title} publishedOn={frontMatter.publishedOn} />
+      <BlogHero title={frontmatter.title} publishedOn={frontmatter.publishedOn} />
       <div className={styles.page}>
         <MDXRemote source={content} />
       </div>
