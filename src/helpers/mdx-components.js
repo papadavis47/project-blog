30b6ffd41859f96cc7ee9904887d6b1c014cac29
John William Davis <jwdavisdeveloper@gmail.com>
2023-08-24 16:24:53 -0700

    exercise 6: finish with help from solution vid

--- src/helpers/mdx-components.js
+++ src/helpers/mdx-components.js
@@ -1,9 +1,11 @@
 import CodeSnippet from '@/components/CodeSnippet/CodeSnippet';
 import DivisionGroupsDemo from '@/components/DivisionGroupsDemo';
+import CircularColorsDemo from '@/components/CircularColorsDemo/CircularColorsDemo';
 
 const COMPONENT_MAP = {
   pre: CodeSnippet,
   DivisionGroupsDemo,
+  CircularColorsDemo,
 };
 
 export default COMPONENT_MAP;
